f097de51e7f2fe7570aad1f53fa67855eed569fd
Syed-8 <irfansyed44730@gmail.com>
2026-04-26 21:32:32 +0530

    Pattern 2

---
+++ Pattern 2/Task 4.java
@@ -0,0 +1,24 @@
+package Pattern2;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int n = 7;
+
+        for (int i = 1; i <= 4; i++) {   
+
+            for (int j = 1; j <= n; j++) {
+
+                if (i == 1) {
+                    System.out.print("* ");
+                }
+                else if (j == i || j == n - i + 1) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
